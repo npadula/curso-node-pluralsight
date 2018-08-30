73193e8716badf85149373cd5d3bb663d870b72c
Nicolas Padula <nicolas.padula@diveria.com>
2018-08-30 15:59:34 -0300

    Almost finished module 4

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+
+
+
+const app = express();
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/"))); 
+app.use('/css',express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))); 
+app.use('/js',express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))); 
+app.use('/js',express.static(path.join(__dirname, "/node_modules/jquery/dist"))); 
+
+
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+
+app.listen(3000, () => {
+    debug(chalk.green("Listening on port 3000..."));
+});
